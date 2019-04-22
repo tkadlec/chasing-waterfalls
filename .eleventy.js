27f66f821142e47e7b9f5582fe92dd37e8a2b598
Tim Kadlec <tim@timkadlec.com>
2019-04-22 12:16:03 -0500

    Manifest

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const CleanCSS = require('clean-css');
 module.exports = function(config) {
 	config.addPassthroughCopy('site/img');
 	config.addPassthroughCopy('site/fonts');
+	config.addPassthroughCopy('site/sw.js');
+	config.addPassthroughCopy('site/manifest.json');
 
 	config.addFilter('cssmin', function(code) {
 		return new CleanCSS({}).minify(code).styles;
